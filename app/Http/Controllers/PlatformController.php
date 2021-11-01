d8a4ab851c2d640537ff7028db57851da900d1d0
Rizal M <mrizalmaulana000@yahoo.com>
2021-11-01 18:02:17 +0700

    create endpoint for update platform data

--- app/Http/Controllers/PlatformController.php
+++ app/Http/Controllers/PlatformController.php
@@ -11,10 +11,14 @@ class PlatformController extends Controller
     public function index()
     {
         try {
-            $platfomFileJson = Storage::get('platform.json');
-            $platformArray = json_decode($platfomFileJson, true);
+            $isFileExist = Storage::exists('platform.json');
+            $allDataPlatform = [];
+            if ($isFileExist) {
+                $platfomFileJson = Storage::get('platform.json');
+                $allDataPlatform = json_decode($platfomFileJson, true);
+            }
 
-            return response()->json(['status' => true, 'data' => $platformArray]);
+            return response()->json(['status' => true, 'data' => $allDataPlatform]);
         } catch (\Exception $e) {
             return response()->json(['status' => false, 'message' => 'Oops something went wrong!']);
         }
@@ -31,7 +35,13 @@ class PlatformController extends Controller
                 'address' => 'required',
             ], [
                 'platform_name.required' => 'platform name is required!',
-                'platform_name.string' => 'platform name must be a string'
+                'platform_name.string' => 'platform name must be a string!',
+                'platform_url.required' => 'platform url is required!',
+                'platform_url.url' => 'platform url must be url!',
+                'phone.required' => 'phone is required!',
+                'email.required' => 'email is required!',
+                'email.email' => 'email must email!',
+                'address.required' => 'address is required!'
             ]);
 
             if ($validator->fails()) {
@@ -67,6 +77,32 @@ class PlatformController extends Controller
 
     public function update(Request $request, $platform)
     {
+        try {
+            $isFileExist = Storage::exists('platform.json');
+            if (!$isFileExist) {
+                return response()->json(['status' => false, 'message' => 'there is no data platform yet!']);
+            }
 
+            $allDataPlatformJson = Storage::get('platform.json');
+            $allDataPlatform = json_decode($allDataPlatformJson, true);
+
+            if (!array_key_exists($platform, $allDataPlatform)) {
+                return response()->json(['status' => false, 'message' => 'data platform ' . $platform . ' does not exists!']);
+            }
+
+            $allDataPlatform[$platform] = [
+                'url' => $request->platform_url ? $request->platform_url : $allDataPlatform[$platform]['url'] ,
+                'phone' => $request->phone ? $request->phone : $allDataPlatform[$platform]['phone'] ,
+                'email' => $request->email ? $request->email : $allDataPlatform[$platform]['email'] ,
+                'address' => $request->address ? $request->address : $allDataPlatform[$platform]['address']
+            ];
+
+            $platformJson = json_encode($allDataPlatform);
+            Storage::put('platform.json', $platformJson);
+
+            return response()->json(['status' => true, 'message' => 'platform ' . $platform . ' updated']);
+        } catch (\Exception $e) {
+            return response()->json(['status' => false, 'message' => 'Oops something went wrong!']);
+        }
     }
 }
